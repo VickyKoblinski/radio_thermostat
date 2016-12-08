6c29904a3a899fd597ca7f794e60874f3d7707fb
Vicky Koblinski <safimoyo@gmail.com>
2016-12-07 21:23:07 -0500

    Displays tstat information

--- cron.js
+++ cron.js
@@ -4,5 +4,5 @@ var i = 0;
 
 new CronJob('* * * * * * *', function(){
   console.log(i++);
-}, null, true, 'America/Los_Angeles');
+}, null, true, 'America/New_York');
  
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
+var request = require('request');
+var address = require('../private/address');
 var router = express.Router();
 
-/* GET home page. */
+/* GET radio thermostat listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request(address+'/tstat/', function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var tstat = JSON.parse(body);
+      var t_temp = tstat.t_heat ? tstat.t_heat : tstat.t_cool;
+      var tmode = ['OFF', 'HEAT', 'COOL', 'AUTO'];
+      var fmode = ['AUTO', 'AUTO/CIRCULATE', 'ON'];
+      var override = ['DISABLED', 'ENABLED'];
+      var hold = ['DISABLED', 'ENABLED'];
+      var tstate = ['OFF', 'HEAT', 'COOL'];
+      var time = dateToString(tstat.time);
+      var t_type_post = ['TEMPORARY', 'ABSOLUTE', 'UNKOWN'];
+
+      res.render('index', { temp: tstat.temp,
+                            t_temp: t_temp,
+                            tmode: tmode[tstat.tmode],
+                            fmode: fmode[tstat.fmode],
+                            override: override[tstat.override],
+                            hold: hold[tstat.hold],
+                            tstate: tstate[tstat.tstate],
+                            time: time,
+                            t_type_post: t_type_post[tstat.t_type_post]
+                          }
+              );
+    } else {
+      res.send('oops, lol.');
+    }
+  })
+
 });
+
+
+function dateToString(date){
+  var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  var am_pm = date.hour > 12 ? 'pm' : 'am'; 
+  var hour = date.hour > 12 ? date.hour-12 : date.hour;
+  var minute = date.minute;
+
+  var str = day[date.day] + ' ' + hour + ':' + minute + am_pm;
+
+  return str;
+}
 
 module.exports = router;
