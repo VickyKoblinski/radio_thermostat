d1087b94ccc05c22ce0ad19fc24adf8301d94ad5
Vicky Koblinski <koblinski@etsu.edu>
2016-12-12 14:15:37 -0500

    Remove commented out lines

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,31 @@
 const express = require('express');
 const request = require('request');
-const address = require('../private/address');
 const router = express.Router();
 const TSTAT = require('../lib/tstat.js');
 
 /* GET radio thermostat listing. */
 router.get('/', (req, res, next) => {
-//  request(address+'/tstat/', (error, response, body) => {
-//    if(!error && response.statusCode == 200){
-//      const TSTAT = JSON.parse(body);
-      const T_TEMP = TSTAT.t_heat ? TSTAT.t_heat : TSTAT.t_cool;
-      const TMODE = ['OFF', 'HEAT', 'COOL', 'AUTO'];
-      const FMODE = ['AUTO', 'AUTO/CIRCULATE', 'ON'];
-      const OVERRIDE = ['DISABLED', 'ENABLED'];
-      const HOLD = ['DISABLED', 'ENABLED'];
-      const TSTATE = ['OFF', 'HEAT', 'COOL'];
-      const TIME = _dateToString(TSTAT.time);
-      const T_TYPE_POST = ['TEMPORARY', 'ABSOLUTE', 'UNKOWN'];
+  const T_TEMP = TSTAT.t_heat ? TSTAT.t_heat : TSTAT.t_cool;
+  const TMODE = ['OFF', 'HEAT', 'COOL', 'AUTO'];
+  const FMODE = ['AUTO', 'AUTO/CIRCULATE', 'ON'];
+  const OVERRIDE = ['DISABLED', 'ENABLED'];
+  const HOLD = ['DISABLED', 'ENABLED'];
+  const TSTATE = ['OFF', 'HEAT', 'COOL'];
+  const TIME = _dateToString(TSTAT.time);
+  const T_TYPE_POST = ['TEMPORARY', 'ABSOLUTE', 'UNKOWN'];
 
-      res.render('index', { temp: TSTAT.temp,
-                            t_temp: T_TEMP,
-                            tmode: TMODE[TSTAT.tmode],
-                            fmode: FMODE[TSTAT.fmode],
-                            override: OVERRIDE[TSTAT.override],
-                            hold: HOLD[TSTAT.hold],
-                            tstate: TSTATE[TSTAT.tstate],
-                            time: TIME,
-                            t_type_post: T_TYPE_POST[TSTAT.t_type_post]
-                          }
-              );
-//    } else {
-//      res.send('oops, lol.');
-//    }
-//  })
+  res.render('index', {
+    temp: TSTAT.temp,
+    t_temp: T_TEMP,
+    tmode: TMODE[TSTAT.tmode],
+    fmode: FMODE[TSTAT.fmode],
+    override: OVERRIDE[TSTAT.override],
+    hold: HOLD[TSTAT.hold],
+    tstate: TSTATE[TSTAT.tstate],
+    time: TIME,
+    t_type_post: T_TYPE_POST[TSTAT.t_type_post]
+  }
+  );
 }
 );
 
